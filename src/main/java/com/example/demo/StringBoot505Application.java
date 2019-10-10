3694da5e3616cea9de551dcdfefa24ee7c8a08d2
prasantib <prasanti.babu@gmail.com>
2019-10-10 12:09:53 -0400

    StringBoot_505

---
+++ src/main/java/com/example/demo/StringBoot505Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot505Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot505Application.class, args);
+    }
+
+}
